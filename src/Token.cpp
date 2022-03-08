07c50d99630374188987e9ef9a1314e14d060364
Marco Cetica <ceticamarco@gmail.com>
2022-03-08 09:44:42 +0100

    Added Token class

---
+++ src/Token.cpp
@@ -0,0 +1,20 @@
+//
+// Created by marco on 3/8/22.
+//
+
+#include "Token.h"
+
+std::string Token::to_string() const {
+    std::string literal_str;
+
+    switch (type) {
+        case(token_type::IDENTIFIER): literal_str = lexeme; break;
+        case(token_type::STRING): literal_str = std::any_cast<std::string>(literal); break;
+        case(token_type::NUMBER): literal_str = std::to_string(std::any_cast<double>(literal)); break;
+        case(token_type::TRUE): literal_str = "true"; break;
+        case(token_type::FALSE): literal_str = "false"; break;
+        default: literal_str = "nil";
+    }
+
+    return std::to_string(static_cast<double>(type)) + " " + lexeme + " " + literal_str;
+}
---
+++ src/Token.h
@@ -0,0 +1,42 @@
+//
+// Created by marco on 3/8/22.
+//
+
+#ifndef MINT_TOKEN_H
+#define MINT_TOKEN_H
+
+#include <any>
+#include <string>
+#include <utility>
+
+enum class token_type {
+    // Single character tokens(i.e., dots, parenthesis, etc.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    // Single or two characters tokens(i.e., !=, ==, >=, etc.)
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+    // Literals(i.e., strings, numbers, etc.)
+    IDENTIFIER, STRING, NUMBER,
+    // Builtin keywords(fn, if, else, etc.)
+    AND, CLASS, ELSE, FALSE, FN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, LET, WHILE,
+    // End of file
+    MINT_EOF
+};
+
+
+class Token {
+public:
+    Token(token_type type, std::any literal, std::string lexeme, unsigned int line)
+        : type(type), literal(std::move(literal)), lexeme(std::move(lexeme)), line(line) {};
+    std::string to_string() const;
+
+    const token_type type;
+    const std::any literal;
+    const std::string lexeme;
+    const unsigned int line;
+};
+
+
+#endif //MINT_TOKEN_H
