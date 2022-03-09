2a78c6242b982898528b67ce26647379ad86c726
Marco Cetica <ceticamarco@gmail.com>
2022-03-09 16:04:16 +0100

    Added Environment class definition

--- src/Environment.h
+++ src/Environment.h
@@ -5,10 +5,21 @@
 #ifndef MINT_ENVIRONMENT_H
 #define MINT_ENVIRONMENT_H
 
+#include <memory>
+#include <any>
+#include <map>
+#include "Token.h"
 
-class Environment {
-
+class Environment : public std::enable_shared_from_this<Environment> {
+public:
+    Environment() : enclosing(nullptr) {};
+    explicit Environment(std::shared_ptr<Environment> enclosing) : enclosing(std::move(enclosing)) {};
+    std::any get(const Token& name);
+    void assign(const Token& name, std::any value);
+    void define(const std::string& name, std::any value);
+private:
+    std::shared_ptr<Environment> enclosing;
+    std::map<std::string, std::any> values;
 };
-
 
 #endif //MINT_ENVIRONMENT_H
