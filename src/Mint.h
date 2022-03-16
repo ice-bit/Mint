4ec0c84f1c730dc784c9adce5d6de29422aa062d
Marco Cetica <ceticamarco@gmail.com>
2022-03-16 10:00:09 +0100

    Fixed a warning

--- src/Mint.h
+++ src/Mint.h
@@ -16,7 +16,7 @@ public:
     static auto error(unsigned int line, const std::string& msg) -> void;
     static auto error(const Token& token, const std::string& msg) -> void;
     static auto runtime_error(const RuntimeError& err) -> void;
-    [[noreturn]] static auto run_prompt() -> void;
+    static auto run_prompt() -> void;
 private:
     static auto run(std::string source) -> void;
     static auto report(unsigned int line, const std::string& pos, const std::string& reason) -> void;
