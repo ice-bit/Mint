50b459041d759c11e68f2e58af5d14523f66f834
Marco Cetica <ceticamarco@gmail.com>
2022-04-05 09:55:04 +0200

    Added unit tests for Lexer class

--- tests/src/test_lexer.cpp
+++ tests/src/test_lexer.cpp
@@ -6,11 +6,25 @@
 
 TEST(LexerTest, TestScanTokens) {
     Lexer lx("let str = \"Hello, World\";");
-    auto tokens = lx.scan_tokens();
-    std::vector<std::string> expected {
-        "let", "str", "="
+    auto actual = lx.scan_tokens();
+
+    const std::vector<Token> expected {
+        Token(token_type::LET, "let", std::string(""), 1),
+        Token(token_type::IDENTIFIER, "str", std::string(""), 1),
+        Token(token_type::EQUAL, "=", std::string(""), 1),
+        Token(token_type::STRING, "\"Hello, World\"", std::string("Hello, World"), 1),
+        Token(token_type::SEMICOLON, ";", std::string(""), 1),
+        Token(token_type::MINT_EOF, "", std::string(""), 1),
+
     };
 
-    for(auto i = tokens.begin(); i != tokens.end(); i++)
-        
+    ASSERT_EQ(actual.size(), expected.size());
+    for(auto i = 0UL; i < expected.size(); ++i) {
+        ASSERT_EQ(expected[i].type, actual[i].type);
+        ASSERT_EQ(expected[i].lexeme, actual[i].lexeme);
+        ASSERT_EQ(expected[i].line, actual[i].line);
+        ASSERT_EQ(
+                std::any_cast<std::string>(expected[i].literal),
+                std::any_cast<std::string>(actual[i].literal));
+    }
 }
